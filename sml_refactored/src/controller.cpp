9c4b53c9b558af53f9e0b0ce5288b8beb747a0d6
Uli Behrje <ulrich.behrje@gmail.com>
2017-02-21 00:11:36 +0100

    Fix compile issue with static_cast<> in constructor

--- sml_refactored/src/controller.cpp
+++ sml_refactored/src/controller.cpp
@@ -5,18 +5,18 @@
 // provide complete type definition
 struct Controller::Fsm : public boost::sml::sm<FsmImpl> {
     explicit Fsm(Controller* ctrl)
-        : boost::sml::sm<FsmImpl>(ctrl) {}
+        : boost::sml::sm<FsmImpl>(static_cast<Controller*>(ctrl)) {}
 };
 
 Controller::Controller()
     : fsm_(std::make_shared<Fsm>(this)) {}
 
 void Controller::startup() {
-//    fsm_.process_event(ev_start{});
+    fsm_->process_event(ev_start{});
 }
 
 void Controller::shutdown() {
-//    fsm_.process_event(ev_stop{});
+    fsm_->process_event(ev_stop{});
 }
 
 void Controller::do_start() const {
